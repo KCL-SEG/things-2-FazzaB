37ce92c0a1ed6b0f8ec46ba87e27a2074663a31c
Fazza Bajwa <fazza.bajwa@kcl.ac.uk>
2022-10-21 19:12:07 +0100

    Creating a things form and displaying it on homepage.

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,10 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
+from django.core.validators import RegexValidator
 
-# Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {'quantity' : forms.NumberInput(),'description' : forms.Textarea()}
